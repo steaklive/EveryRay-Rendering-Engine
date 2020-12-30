dec781b12c1f1d3cd36c2cdbd398f7a4322fdd04
steaklive <steaklive@gmail.com>
2020-12-30 15:21:45 +0100

    moved volumetric clouds to separate class

--- source/Game/TestSceneDemo.h
+++ source/Game/TestSceneDemo.h
@@ -27,6 +27,7 @@ namespace Library
 	class Terrain;
 	class Foliage;
 	class Scene;
+	class VolumetricClouds;
 }
 
 namespace DirectX
@@ -84,7 +85,6 @@ namespace Rendering
 		TestSceneDemo(const TestSceneDemo& rhs);
 		TestSceneDemo& operator=(const TestSceneDemo& rhs);
 
-		void DrawVolumetricClouds(const GameTime& gameTime);
 		void UpdateStandardLightingPBRMaterialVariables(const std::string& objectName, int meshIndex);
 		void UpdateDeferredPrepassMaterialVariables(const std::string& objectName, int meshIndex);
 		void UpdateShadow0MaterialVariables(const std::string & objectName, int meshIndex);
@@ -105,6 +105,7 @@ namespace Rendering
 		GBuffer* mGBuffer;
 		PostProcessingStack* mPostProcessingStack;
 		std::vector<Foliage*> mFoliageCollection;
+		VolumetricClouds* mVolumetricClouds;
 
 		ID3D11ShaderResourceView* mIrradianceTextureSRV;
 		ID3D11ShaderResourceView* mRadianceTextureSRV;
@@ -116,34 +117,5 @@ namespace Rendering
 		float mWindGustDistance = 1.0f;
 		//Terrain* mTerrain;
 
-		CustomRenderTarget* mVolumetricCloudsRenderTargetCS = nullptr;
-		FullScreenRenderTarget* mVolumetricCloudsRenderTarget = nullptr;
-		FullScreenRenderTarget* mVolumetricCloudsCompositeRenderTarget = nullptr;
-		FullScreenRenderTarget* mVolumetricCloudsBlurRenderTarget = nullptr;
-		ID3D11ComputeShader* VCMainCS = nullptr;
-		ID3D11PixelShader* VCMainPS = nullptr;
-		ID3D11PixelShader* VCCompositePS = nullptr;
-		ID3D11PixelShader* VCBlurPS = nullptr;
-
-		ID3D11ShaderResourceView* mCloudTextureSRV = nullptr;
-		ID3D11ShaderResourceView* mWeatherTextureSRV = nullptr;
-		ID3D11ShaderResourceView* mWorleyTextureSRV = nullptr;
-
-		ID3D11SamplerState* mCloudSS = nullptr;
-		ID3D11SamplerState* mWeatherSS = nullptr;
-
-		float mCloudsCrispiness = 40.0f;
-		float mCloudsCurliness = 0.2f;
-		float mCloudsCoverage = 0.5f;
-		float mCloudsAmbientColor[3] = { 121.0f/255.0f, 133.0f/255.0f, 138.0f/255.0f };
-		float mCloudsWindSpeedMultiplier = 1000.0f;
-		//float mCloudsLayerInnerHeight = 5000.0f;
-		//float mCloudsLayerOuterHeight = 17000.0f;
-		float mCloudsLightAbsorption = 0.002f;
-
-		bool mUseComputeShaderVolumetricClouds = true;
-
-		ConstantBuffer<VolumetricCloudsData::FrameCB> mVolumetricCloudsFrameConstantBuffer;
-		ConstantBuffer<VolumetricCloudsData::CloudsCB> mVolumetricCloudsCloudsConstantBuffer;
 	};
 }
